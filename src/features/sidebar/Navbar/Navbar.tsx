a795b249de2839b30f922236ba81dab794d960ea
Daniil Fefelov <fefelov.danil@gmail.com>
2022-12-15 11:14:06 +0600

    feat: added link to user page

--- src/features/sidebar/Navbar/Navbar.tsx
+++ src/features/sidebar/Navbar/Navbar.tsx
@@ -4,23 +4,26 @@ import {NavLink} from "react-router-dom";
 import {PATHS} from "common/routes/Pages";
 
 export const Navbar = () => {
-    return (
-        <nav className={`${s.sidebarEl} ${s.navbar}`}>
-            <div className={s.item}>
-                <NavLink to={PATHS.PROFILE}>Профиль</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={PATHS.MESSAGES}>Сообщения</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={PATHS.NEWS}>Новости</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={PATHS.MUSIC}>Музыка</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={PATHS.SETTINGS}>Настройки</NavLink>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className={`${s.sidebarEl} ${s.navbar}`}>
+      <div className={s.item}>
+        <NavLink to={PATHS.PROFILE}>Профиль</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to={PATHS.MESSAGES}>Сообщения</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to={PATHS.NEWS}>Новости</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to={PATHS.MUSIC}>Музыка</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to={PATHS.USERS}>Пользователи</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to={PATHS.SETTINGS}>Настройки</NavLink>
+      </div>
+    </nav>
+  )
 }
