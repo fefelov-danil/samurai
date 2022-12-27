9211a6f09b3427b9c2aea803a8fcfe882c2fb3ea
Daniil Fefelov <fefelov.danil@gmail.com>
2022-12-27 23:33:25 +0600

    feat: login

---
+++ src/features/profile/login/Login.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import s from './Login.module.css'
+import {SubmitHandler, useForm} from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import {Button} from "common/button/Button";
+import {loginSchema} from "utils/schemas/loginValidation";
+import {InputText} from "common/inputText/InputText";
+import {InputPassword} from "common/inputPassword/InputPassword";
+import {Checkbox} from "common/checkbox/Checkbox";
+
+export const Login = () => {
+  const {
+    register,
+    formState: {errors},
+    handleSubmit
+  } = useForm<loginValues>({mode: "onTouched", resolver: yupResolver(loginSchema)})
+
+  const onSubmit: SubmitHandler<loginValues> = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <div className={s.login}>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          <InputText {...register('email')} />
+          {errors?.email && <p className={s.error}>{errors.email.message}</p>}
+        </label>
+        <label>
+          <InputPassword {...register('password')} />
+          {errors?.password && <p className={s.error}>{errors.password.message}</p>}
+        </label>
+        <Checkbox {...register('rememberMe')}>Запомнить меня</Checkbox>
+        <Button type={"submit"}>Отправить</Button>
+      </form>
+    </div>
+  );
+};
+
+//----------- TYPES -----------//
+type loginValues = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
