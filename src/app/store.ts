4339d292c3cf8e7ccdd638604a86dbdf3cec7704
Daniil Fefelov <fefelov.danil@gmail.com>
2022-12-27 23:24:40 +0600

    feat: configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
-import {AnyAction, applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import {AnyAction, combineReducers} from "redux";
 import {dialogsReducer} from "features/dialogs/dialogs-reducer";
 import {profileReducer} from "features/profile/profile-reducer";
 import {sidebarReducer} from "features/sidebar/sidebar-reducer";
 import thunk, {ThunkDispatch} from "redux-thunk";
 import {usersReducer} from "features/users/users-reducer";
+import {configureStore} from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
     profile: profileReducer,
@@ -12,7 +13,10 @@ const rootReducer = combineReducers({
     users: usersReducer
 })
 
-export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
+})
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>
