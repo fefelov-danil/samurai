409f0b5a36bb2fdaaaaa5fd311f81d706a5ea2bb
Daniil Fefelov <fefelov.danil@gmail.com>
2023-01-13 22:32:24 +0600

    feat: added instance api for photo

--- src/api/instance-api.ts
+++ src/api/instance-api.ts
@@ -7,3 +7,12 @@ export const instance = axios.create({
     'API-KEY': '4e9b0636-58a9-499c-aa39-97e8d2fc85f8'
   }
 })
+
+export const instancePhoto = axios.create({
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  withCredentials: true,
+  headers: {
+    'API-KEY': '4e9b0636-58a9-499c-aa39-97e8d2fc85f8',
+    "Content-Type": "multipart/form-data"
+  }
+})
