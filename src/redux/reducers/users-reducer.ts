88bfb0dcfbc13b81781da390d8734059a9fac443
Daniil Fefelov <fefelov.danil@gmail.com>
2023-01-18 11:58:17 +0600

    change: redux toolkit for users-reducer.ts

--- src/redux/reducers/users-reducer.ts
+++ src/redux/reducers/users-reducer.ts
@@ -1,45 +1,74 @@
-const usersInitialState = {
-  users: [] as UserType[]
-}
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {setAppStatus} from "redux/reducers/app-reducer";
+import {GetUsersParametersType, UsersInitialState} from "api/uaersApi/types";
+import {networkError, serverError} from "utils/error-utils";
+import {AxiosError} from "axios";
+import {usersAPI} from "api/uaersApi/users-api";
+import {profileApi} from "api/profileApi/profile-api";
 
-export const usersReducer =
-  (state: UsersInitialStateType = usersInitialState, action: UsersActionsType): UsersInitialStateType => {
-    switch (action.type) {
-      case "users/SET-USERS":
-        return {...state, users: action.users}
-      case "users/FOLLOWED":
-        return {...state, users: state.users.map(u => u.id === action.userId ? {...u, followed: !u.followed} : u)}
-      default:
-        return state
+export const getUsers = createAsyncThunk('users/getUsers',
+  async (params: GetUsersParametersType, {dispatch, rejectWithValue}) => {
+    dispatch(setAppStatus('loading'))
+    try {
+      const res = await usersAPI.getUsers(params)
+      if (res.data.error === null) {
+        return res.data
+      } else {
+        serverError(dispatch, res.data.error)
+        return rejectWithValue(null)
+      }
+    } catch (e) {
+      networkError(e as Error | AxiosError<{ error: string }>, dispatch)
+      return rejectWithValue(null)
+    } finally {
+      dispatch(setAppStatus('idle'))
     }
+})
+
+export const getUserProfile = createAsyncThunk('users/getUserProfile',
+  async (myId: number, {dispatch, rejectWithValue}) => {
+    dispatch(setAppStatus('loading'))
+    try {
+      const res = await profileApi.getProfile(myId)
+      return res.data
+    } catch (e) {
+      networkError(e as Error | AxiosError<{ error: string }>, dispatch)
+      return rejectWithValue(null)
+    } finally {
+      dispatch(setAppStatus('idle'))
+    }
+  })
+
+export const getUserProfileStatus = createAsyncThunk('users/getUserProfileStatus',
+  async (myId: number, {dispatch, rejectWithValue}) => {
+    dispatch(setAppStatus('loading'))
+    try {
+      const res = await profileApi.getStatus(myId)
+      return res.data
+    } catch (e) {
+      networkError(e as Error | AxiosError<{ error: string }>, dispatch)
+      return rejectWithValue(null)
+    } finally {
+      dispatch(setAppStatus('idle'))
+    }
+  })
+
+const sliceUsers = createSlice({
+  name: 'users',
+  initialState: {} as UsersInitialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getUsers.fulfilled, (state, action) => {
+      state.totalCount = action.payload.totalCount
+      state.items = action.payload.items
+    })
+    builder.addCase(getUserProfile.fulfilled, (state, action) => {
+      state.userProfile = action.payload
+    })
+    builder.addCase(getUserProfileStatus.fulfilled, (state, action) => {
+      state.userStatus = action.payload
+    })
   }
+})
 
-export const setUsersAC = (users: UserType[]) => {
-  return {type: 'users/SET-USERS', users} as const
-}
-
-export const followedAC = (userId: string) => {
-  return {type: 'users/FOLLOWED', userId} as const
-}
-
-
-// Types
-type UsersInitialStateType = typeof usersInitialState
-
-export type UserType = {
-  id: string
-  photoUrl: string
-  followed: boolean
-  fullName: string
-  status: string
-  location: LocationType
-}
-
-type LocationType = {
-  city: string
-  country: string
-}
-
-type UsersActionsType =
-  | ReturnType<typeof setUsersAC>
-  | ReturnType<typeof followedAC>
+export const usersReducer = sliceUsers.reducer
