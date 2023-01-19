4eb11cc45577e8ff04260bf29fc06aa0259f32f5
Daniil Fefelov <fefelov.danil@gmail.com>
2023-01-19 22:59:29 +0600

    feat: follow API

--- src/api/uaersApi/users-api.ts
+++ src/api/uaersApi/users-api.ts
@@ -1,8 +1,18 @@
 import {GetUsersParametersType, UsersType} from "api/uaersApi/types";
 import {instance} from "api/instance-api";
+import {ResponseType} from "api/profileApi/types";
 
 export const usersAPI = {
   getUsers(data: GetUsersParametersType) {
-    return instance.get<UsersType>('users', { params: { ...data } })
+    return instance.get<UsersType>(`users`, { params: { ...data } })
+  },
+  isFollowed(userId: number) {
+    return instance.get<ResponseType>(`follow/${userId}`)
+  },
+  follow(userId: number) {
+    return instance.post<ResponseType>(`follow/${userId}`)
+  },
+  unFollow(userId: number) {
+    return instance.delete<ResponseType>(`follow/${userId}`)
   }
 }
