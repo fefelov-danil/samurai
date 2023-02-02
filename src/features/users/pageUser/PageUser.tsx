b48e104325b5589aef41512e855d7f6898be133d
Daniil Fefelov <fefelov.danil@gmail.com>
2023-02-02 12:26:46 +0600

    fix: rerender bag

--- src/features/users/pageUser/PageUser.tsx
+++ src/features/users/pageUser/PageUser.tsx
@@ -1,28 +1,29 @@
 import React, {useEffect} from 'react';
 import {useParams} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "utils/hooks";
-import {getUserProfile, getUserProfileStatus} from "redux/reducers/users-reducer";
+import {getUserProfile} from "redux/reducers/users-reducer";
 import {ProfileInfo} from "features/profile/profileInfo/ProfileInfo";
 
 export const PageUser = () => {
   const dispatch = useAppDispatch()
-  const appStatus = useAppSelector(state => state.app.appStatus)
   const userProfile = useAppSelector(state => state.users.userProfile)
   const userStatus = useAppSelector(state => state.users.userStatus)
   const { id } = useParams()
 
   useEffect(() => {
-    if (id ) {
+    if (id) {
       dispatch(getUserProfile(+id))
-      dispatch(getUserProfileStatus(+id))
     }
   }, [id])
 
-  if (appStatus === 'loading' || userProfile === undefined) {
+  if (!userProfile?.fullName) {
     return <h1>Loading...</h1>
   }
 
   return (
-    <ProfileInfo myOrUserProfile={'user'} profile={userProfile} status={userStatus} />
+    <ProfileInfo
+      myOrUserProfile={'user'}
+      profile={userProfile}
+      status={userStatus}/>
   );
 };
