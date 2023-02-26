0043cf2c923ba325bbcf232cabb298cc7b37fa97
Daniil Fefelov <fefelov.danil@gmail.com>
2023-02-26 23:48:51 +0600

    feat: added pagination in Users

--- src/features/users/Users.tsx
+++ src/features/users/Users.tsx
@@ -5,15 +5,17 @@ import {clearUsers, getUsers} from "redux/reducers/users-reducer";
 import {User} from "features/users/user/User";
 import {AllOrFriends} from "features/users/filters/all-or-friends/AllOrFriends";
 import {InputSearch} from "features/users/filters/search/InputSearch";
+import {Pagination} from "common/pagination/Pagination";
 
 export const Users = () => {
   const dispatch = useAppDispatch()
   const users = useAppSelector(state => state.users.items)
+  const totalCount = useAppSelector(state => state.users.totalCount)
 
   const [isFriends, setIsFriends] = useState(true)
   const [searchValue, setSearchValue] = useState('')
   const [page, setPage] = useState(1)
-  const [count, setCount] = useState(12)
+  const [count, setCount] = useState(16)
 
   useEffect(() => {
     dispatch(getUsers({count: count, page: page, term: searchValue, friend: isFriends}))
@@ -21,14 +23,24 @@ export const Users = () => {
     return () => {
       dispatch(clearUsers())
     }
-  }, [isFriends, searchValue])
+  }, [isFriends, searchValue, page, count])
 
   const switchUsers = (isFriends: boolean) => {
     setIsFriends(isFriends)
+    setPage(1)
+    setCount(16)
   }
 
   const changeInputSearch = (value: string) => {
     setSearchValue(value)
+  }
+
+  const changeCountItemsPerPage = (items: number) => {
+    setCount(items)
+  }
+
+  const changeCurrentPage = (page: number) => {
+    setPage(page)
   }
 
   return (
@@ -44,6 +56,12 @@ export const Users = () => {
       <div className={s.userColumns}>
         {users?.map(u => <User key={u.id} user={u}/>)}
       </div>
+      <Pagination
+        currentPage={page}
+        countItemsPerPage={count}
+        changeCountItemsPerPage={changeCountItemsPerPage}
+        totalCount={totalCount}
+        changeCurrentPage={changeCurrentPage} />
     </div>
   );
 };
