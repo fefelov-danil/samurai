2ccc008c0fc3f2871ef030a13146276f188699ee
Daniil Fefelov <fefelov.danil@gmail.com>
2023-04-01 13:02:20 +0400

    test commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -35,7 +35,7 @@ export const App = () => {
     <div className="app-wrapper">
       {isLoggedIn && (
         <>
-          <Header/>
+          <Header/>1
           <Sidebar/>
         </>
       )}
